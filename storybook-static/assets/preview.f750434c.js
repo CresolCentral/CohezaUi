a734a84dc4d65b9f5f1b63972fa35b44f83c639f
Carlos Zanchet <carlos.zanchet@cresolcentral.coop.br>
2022-10-25 10:14:49 -0300

    alteracao deploy git

---
+++ storybook-static/assets/preview.f750434c.js
@@ -0,0 +1,2 @@
+import{t as o}from"./index.21d417a9.js";import"./iframe.ce4adc09.js";import"./es.map.constructor.e72f1aa4.js";import"./es.number.to-fixed.ad9e682e.js";import"./index.d7b398fd.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{expanded:!0,matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
+//# sourceMappingURL=preview.f750434c.js.map
